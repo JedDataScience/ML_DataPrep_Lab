6d16c675b4e8a1aa4cec34af8c9897a47b93d477
Josh Detmer <joshuadetmer@gmail.com>
2026-02-04 03:14:56 +0000

    More changes

--- Code.py
+++ Code.py
@@ -19,8 +19,9 @@ import requests  # For HTTP requests to download data
 college_completions_url = "https://raw.githubusercontent.com/UVADS/DS-3021/main/data/cc_institution_details.csv"
 college = pd.read_csv(college_completions_url)
 
-## Question that can be answered with this data:
-# what are the different types of institutions that have the highest completion rates and what variables are most correlated with completion rates?
+## Question that can be answered with this data using classification or regression models:
+# Can we predict the graduation rate of a college based on its characteristics such as size, control 
+# The independent business metric for my problem is the accuracy of predicting whether a college has a high or low graduation rate based on its features.
 
 
 
@@ -29,10 +30,9 @@ college.info()
 # %%
 
 # %% 
-#Select relevant features for analysis: I chose these columns based on their potential impact on college completion rat
+#Select relevant features for analysis: I chose these columns based on the description of the data and what I thought would be relevant to the question I want to answer.
 #The Columns that I selected dont have a lot of missing values so I decided to keep them
-columns_to_drop = [,]
-college = college.iloc[:, [,]]
+college = college.iloc[:, [3,4,5,6,7,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39]]
 college.info()
 
 
@@ -69,6 +69,62 @@ college #notice the difference in the range of values for the numeric variables
 # %%
 # Lets do the one hot encoding for the categorical variables
 college = pd.get_dummies(college, columns=['level','control'])
-college.info()
 
 # %%
+# I want ot get the prevalence of the target variable, which is the graduation rate, to see if it is imbalanced
+college.boxplot(column='grad_100_value', vert=False, grid=False)
+college.grad_100_value.describe()
+# I notice that the upper quartile of values is above 0.4365, which means that 75% of the colleges have a graduation rate below 43.65%. 
+# This suggests that the target variable is somewhat imbalanced, with a majority of colleges having lower graduation rates. 
+# This is important to keep in mind when building classification models, as it may affect the performance and require techniques to address the imbalance.
+
+
+# %%
+college['grad_100_value_f'] = pd.cut(college.grad_100_value, bins = [-1,0.4365,1], labels =[0,1])
+college_clean = college.dropna(subset=['grad_100_value_f'])
+college 
+# I created a new binary variable called grad_100_value_f that categorizes the graduation rate into two groups: 0 for colleges with a graduation rate of 43.65% or lower, and 1 for colleges with a graduation rate above 43.65%. 
+# This will allow me to use this variable as a target for classification models, where I can predict whether a college has a high or low graduation rate based on its characteristics.
+
+# %%
+#So now let's check the prevalence 
+prevalence = college_clean.grad_100_value_f.value_counts()[1]/len(college_clean.grad_100_value_f)
+print(f"The prevalence of colleges with a graduation rate above 43.65% is: {prevalence:.2f}")
+# The prevalence of colleges with a graduation rate above 43.65% is: 0.25, which means that 25% of the colleges in the dataset have a graduation rate above 43.65%.
+
+
+# %%
+# I want to know partition the data into training and testing sets.
+Train, Test = train_test_split(college_clean, train_size=2600, stratify=college_clean.grad_100_value_f)
+
+
+# %%
+print(Train.shape)
+print(Test.shape)
+
+# %%
+# Now we can use it again to create a tunning set. 
+Tune, Test = train_test_split(Test,  train_size = .25, stratify= Test.grad_100_value_f)
+print(Tune.shape)
+print(Test.shape)
+
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
